c7c16251a82ba0e9100ae1bb8b4a4e0f0b2418d5
BenB116 <benb116@gmail.com>
2014-03-03 00:26:38 -0500

    Added Now Playing block and separate JS file

---
+++ scripts.js
@@ -0,0 +1,48 @@
+function SmoothScroll(){
+	$(function() {
+		  $('a[href*=#]:not([href=#])').click(function() {
+		    if (location.pathname.replace(/^\//,'') == this.pathname.replace(/^\//,'') && location.hostname == this.hostname) {
+
+		      var target = $(this.hash);
+		      target = target.length ? target : $('[name=' + this.hash.slice(1) +']');
+		      if (target.length) {
+		        $('html,body').animate({
+		          scrollTop: target.offset().top
+		        }, 500);
+		        return false;
+		      }
+		    }
+		  });
+		});
+}
+
+function ShowArrow(){
+	var $logo = $('#UpArrow');
+	$(document).scroll(function() {
+	    $logo.css({opacity: $(this).scrollTop() > 700? "1":"0"});
+	});
+}
+
+function GetLastFMData(){
+	if (window.XMLHttpRequest)
+	  {// code for IE7+, Firefox, Chrome, Opera, Safari
+	  xmlhttp=new XMLHttpRequest();
+	  }
+	else
+	  {// code for IE6, IE5
+	  xmlhttp=new ActiveXObject("Microsoft.XMLHTTP");
+	  }
+	xmlhttp.open("GET","http://ws.audioscrobbler.com/2.0/?method=user.getrecenttracks&user=Benb116&api_key=d6b2ab49b0a34737be62158c0ddfd7c5&limit=1",false);
+	xmlhttp.send();
+	xmlDoc=xmlhttp.responseXML;
+
+	/*document.getElementById("NowRecent").innerHTML=*/
+	var playval = xmlDoc.getElementsByTagName("track")[0].attributes.getNamedItem("nowplaying").value;
+	if(playval == "true") {
+		document.getElementById("NowRecent").innerHTML="Now Playing";
+	}
+	document.getElementById("TrackName").innerHTML=xmlDoc.getElementsByTagName("name")[0].childNodes[0].nodeValue;
+	document.getElementById("TrackArtist").innerHTML=xmlDoc.getElementsByTagName("artist")[0].childNodes[0].nodeValue;
+	document.getElementById("TrackArt").src=xmlDoc.getElementsByTagName("image")[3].childNodes[0].nodeValue;
+	document.getElementById("TrackLink").href=xmlDoc.getElementsByTagName("url")[0].childNodes[0].nodeValue;
+}
