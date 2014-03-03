7739984f65dba7a55197390db180950cee5a7b7d
BenB116 <benb116@gmail.com>
2014-03-03 01:11:39 -0500

    Fixed Recently Played bug

--- scripts.js
+++ scripts.js
@@ -23,6 +23,26 @@ function ShowArrow(){
 	});
 }
 
+function GetRecentPlay(){
+	if (window.XMLHttpRequest)
+	  {// code for IE7+, Firefox, Chrome, Opera, Safari
+	  xmlhttp=new XMLHttpRequest();
+	  }
+	else
+	  {// code for IE6, IE5
+	  xmlhttp=new ActiveXObject("Microsoft.XMLHTTP");
+	  }
+	xmlhttp.open("GET","http://ws.audioscrobbler.com/2.0/?method=user.getrecenttracks&user=Benb116&api_key=d6b2ab49b0a34737be62158c0ddfd7c5&limit=1",false);
+	xmlhttp.send();
+	xmlDoc=xmlhttp.responseXML;
+
+	document.getElementById("TrackName").innerHTML=xmlDoc.getElementsByTagName("name")[0].childNodes[0].nodeValue;
+	document.getElementById("TrackArtist").innerHTML=xmlDoc.getElementsByTagName("artist")[0].childNodes[0].nodeValue;
+	document.getElementById("TrackArt").src=xmlDoc.getElementsByTagName("image")[3].childNodes[0].nodeValue;
+	document.getElementById("TrackArt").className="Other";
+	document.getElementById("TrackLink").href=xmlDoc.getElementsByTagName("url")[0].childNodes[0].nodeValue;
+}
+
 function GetLastFMData(){
 	if (window.XMLHttpRequest)
 	  {// code for IE7+, Firefox, Chrome, Opera, Safari
@@ -40,9 +60,12 @@ function GetLastFMData(){
 	var playval = xmlDoc.getElementsByTagName("track")[0].attributes.getNamedItem("nowplaying").value;
 	if(playval == "true") {
 		document.getElementById("NowRecent").innerHTML="Now Playing";
+	} else {
+		document.getElementById("NowRecent").innerHTML="Recently Played";
 	}
 	document.getElementById("TrackName").innerHTML=xmlDoc.getElementsByTagName("name")[0].childNodes[0].nodeValue;
 	document.getElementById("TrackArtist").innerHTML=xmlDoc.getElementsByTagName("artist")[0].childNodes[0].nodeValue;
 	document.getElementById("TrackArt").src=xmlDoc.getElementsByTagName("image")[3].childNodes[0].nodeValue;
+	document.getElementById("TrackArt").className="pulse-grow";
 	document.getElementById("TrackLink").href=xmlDoc.getElementsByTagName("url")[0].childNodes[0].nodeValue;
 }
